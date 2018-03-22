bbdbd3e935923bc9769548f11ac9c74bfa4f40f4
yutianzuo <yutianzuo@meituan.com>
2018-03-22 14:49:57 +0800

    delete idea

--- app/src/main/java/com/github/yutianzuo/camerademo/MainActivity.java
+++ app/src/main/java/com/github/yutianzuo/camerademo/MainActivity.java
@@ -1,13 +1,36 @@
 package com.github.yutianzuo.camerademo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv1 = findViewById(R.id.txt1);
+        tv1.setOnClickListener(this);
+
+        TextView tv2 = findViewById(R.id.txt2);
+        tv2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+        switch (view.getId()) {
+            case R.id.txt1:
+                intent = new Intent(this, CustomCamera.class);
+                startActivity(intent);
+                break;
+            case R.id.txt2:
+                Toast.makeText(this, "coming soon", Toast.LENGTH_LONG).show();
+                break;
+        }
     }
 }
