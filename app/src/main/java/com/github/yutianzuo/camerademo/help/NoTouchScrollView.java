872c1055e88da400f0faba50cfa1e3020a2caf1d
yutianzuo <yutianzuo@meituan.com>
2018-03-26 11:04:45 +0800

    ui update

---
+++ app/src/main/java/com/github/yutianzuo/camerademo/help/NoTouchScrollView.java
@@ -0,0 +1,27 @@
+package com.github.yutianzuo.camerademo.help;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.ScrollView;
+
+public class NoTouchScrollView extends ScrollView {
+
+	public NoTouchScrollView(final Context context, final AttributeSet attrs)
+	{
+		super(context, attrs);
+	}
+	public NoTouchScrollView(Context context)
+	{
+		super(context);
+	}
+
+	public NoTouchScrollView(Context context, AttributeSet attrs, int ar)
+	{
+		super(context, attrs, ar);
+	}
+	@Override
+	public boolean onTouchEvent(MotionEvent ev) {
+		return false;
+	}
+}
